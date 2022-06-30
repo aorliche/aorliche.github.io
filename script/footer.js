d61d027de106b45aead26baf4a7a55b938d333b4
Anton Orlichenko <aorliche@gmail.com>
2022-06-30 14:59:32 -0400

    Navbar, basic posts (2nd Post), and footer'

---
+++ script/footer.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', e => {
+    const footer = document.querySelector('#footer');
+    const p = document.createElement('p');
+    const a = document.createElement('a');
+    a.innerText = 'Contact me';
+    a.href = 'https://hunimal.org/Contact.php';
+    p.innerHTML = "&#169; 2022 Anton Orlichenko<br><iframe src='https://hunimal.org/aorliche/counter.php' title='Counter' style='border: none; width: 50px; height: 11px;'></iframe> visitors since June 2022<br>";
+    p.appendChild(a);
+    footer.appendChild(p);
+});
--- script/posts.js
+++ script/posts.js
@@ -3,25 +3,87 @@ window.addEventListener('load', e => {
     const postsDiv = document.querySelector('#posts');
     
     function populatePosts() {
-        posts.forEach(post => console.log(post.date));
+        let gotFirst = false;
         posts.forEach(post => post.date = new Date(post.date));
         posts.sort((a,b) => b.date-a.date);
+        for (let i=0; i<posts.length; i++) {
+            posts[i].n = posts.length-i-1;
+        }
         if (posts.length == 0) {
             postsDiv.innerHTML = "<h2>No posts yet</h2>";
         } else {
-            posts.forEach(post => {
+            const pinned = posts.filter(post => post.pinned);
+            const unpinned = posts.filter(post => !post.pinned);
+            pinned.concat(unpinned).forEach(post => {
+                if (!gotFirst && !post.pinned) {
+                    const h4 = document.createElement('h4');
+                    h4.innerText = 'Recent Posts';
+                    postsDiv.appendChild(h4);
+                    gotFirst = true;
+                }
                 const pdiv = document.createElement('div');
                 pdiv.classList.add('post');
+                const date = document.createElement('span');
+                date.classList.add('date');
+                date.innerText = post.date.toDateString();
                 const h2 = document.createElement('h2');
-                h2.innerText = post.title;
-                const below = document.createElement('div');
-                below.innerText = `Posted by ${post.author} on ${post.date.toString()}`;
+                h2.innerText = post.title + ' ';
+                if (post.pinned) {
+                    const pin = document.createElement('span');
+                    const img = document.createElement('img');
+                    pin.classList.add('pin');
+                    pin.innerText = '[Pinned] ';
+                    img.src = '/images/pin.png';
+                    img.style.height = '14px';
+                    img.style.top = '6px';
+                    img.style.position = 'relative';
+                    //h2.prepend(pin);
+                    h2.prepend(img);
+                }
+                h2.prepend(date);
+                //const below = document.createElement('div');
+                //below.innerText = `Posted by ${post.author} on ${post.date.toString()}`;
+                const showBefore = document.createElement('a');
+                showBefore.classList.add('pin');
+                showBefore.innerText = 'Show';
+                showBefore.href = '#';
+                h2.appendChild(showBefore);
                 const body = document.createElement('div');
                 body.innerHTML = post.body;
+                if (!post.pinned) {
+                    body.appendChild(document.createElement('hr'));
+                    body.prepend(document.createElement('hr'));
+                }
                 pdiv.appendChild(h2);
-                pdiv.appendChild(below);
+                //pdiv.appendChild(below);
                 pdiv.appendChild(body);
                 postsDiv.appendChild(pdiv);
+                if (post.pinned) {
+                    showBefore.innerText = 'Hide';
+                } else {
+                    body.style.display = 'none';
+                }
+                const hideAfter = document.createElement('a');
+                const hideAfterP = document.createElement('p');
+                hideAfter.innerText = 'Hide';
+                hideAfter.href = '#';
+                hideAfterP.appendChild(hideAfter);
+                body.appendChild(hideAfterP);
+                showBefore.addEventListener('click', e => {
+                    e.preventDefault();
+                    if (showBefore.innerText == 'Show') {
+                        body.style.display = 'block';
+                        showBefore.innerText = 'Hide';
+                    } else {
+                        body.style.display = 'none';
+                        showBefore.innerText = 'Show';
+                    }
+                });
+                hideAfterP.addEventListener('click', e => {
+                    e.preventDefault();
+                    body.style.display = 'none';
+                    showBefore.innerText = 'Show';
+                });
             });
         }
     }
