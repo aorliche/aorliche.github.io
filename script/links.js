dc0c5b29c0a37f957aefc36201bafa0991b52ffb
Anton Orlichenko <aorliche@gmail.com>
2022-08-13 10:57:35 -0500

    Updated main page layout - art page got worse

--- script/links.js
+++ script/links.js
@@ -13,21 +13,21 @@ window.addEventListener('load', e => {
         const ul = cat.parentNode.querySelector('ul');
         cat.addEventListener('mouseenter', e => {
             if (openul) {
-                openul.style.transform = 'translateY(-200px)';
+                openul.style.transform = 'translateY(-140px)';
             }
             ul.style.transform = 'translateY(0px)';
             openul = ul;
         });
         cat.addEventListener('mouseout', e => {
             if (!cursorInElement(ul, e)) {
-                ul.style.transform = 'translateY(-200px)';
+                ul.style.transform = 'translateY(-140px)';
                 openul = null;
             }
         });
         ul.addEventListener('mouseout', e => {
             // position: absolute takes ul out of flow?
             if (!cursorInElement(cat, e) && !cursorInElement(ul, e)) {
-                ul.style.transform = 'translateY(-200px)';
+                ul.style.transform = 'translateY(-140px)';
                 openul = null;
             }                
         });
