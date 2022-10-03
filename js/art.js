c44f64d5c4608974113d4b48d4656a09ee3e51f8
Anton Orlichenko <aorliche@gmail.com>
2022-10-02 19:13:25 -0500

    Made good gallery and added Mr. Pig, fixed triplets quote

--- js/art.js
+++ js/art.js
@@ -1,10 +1,30 @@
 var $ = q => document.querySelector(q);
-var $$ = q => document.querySelectorAll(q);
+var $$ = q => [...document.querySelectorAll(q)];
+
+function setImg(thumb) {
+    const img = thumb.querySelector('img');
+    $('#big img').src = img.src;
+    $('#big h2').innerText = img.alt; 
+}
 
 window.addEventListener('load', e => {
-    [...$$('.thumb')].forEach(div => {
-        div.addEventListener('click', e2 => {
-            location.href = div.querySelector('img').src;
+    [...$$('.thumb')].forEach(thumb => {
+        thumb.addEventListener('click', elt => {
+            setImg(thumb);
         });
+    });
+    setImg($('.thumb'));
+    
+    let storedHash = '#' + $$('.gallery').filter(g => !g.classList.contains('hidden'))[0].id;
+    window.addEventListener('hashchange', e => {
+        e.preventDefault();
+        if (location.hash != storedHash) {
+            const newHash = location.hash.split('-')[0];
+            $(`${newHash}`).classList.remove('hidden');
+            $(`${storedHash}`).classList.add('hidden');
+            $('#big img').src = '';
+            $('#big h2').innerText = '';
+            storedHash = newHash;
+        }
     });
 });
