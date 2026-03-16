e148a935466ac469ddcb1206cf3fddd36c342d45
Anton Orlichenko <aorliche@gmail.com>
2026-03-16 15:36:18 -0400

    Single display art galleries

--- js/art.js
+++ js/art.js
@@ -21,13 +21,16 @@ window.addEventListener('load', e => {
     fetch('/art-manifest.json')
     .then(resp => resp.json())
     .then(json => {
+		const galleriesJs = []; 
         for (let gname in json) {
             const nospace = gname.replace(/ /g, ''); 
             const h2 = document.createElement('h2');
+			const hr = document.createElement('hr');
             h2.innerText = gname;
             h2.id = gname;
             const gdiv = document.createElement('div');
             gdiv.classList.add('gallery');
+			galleriesJs.push({h2, hr, gdiv});
             const imgInfos = json[gname];
             for (let i=0; i<imgInfos.length; i++) {
                 const info = imgInfos[i];
@@ -45,7 +48,7 @@ window.addEventListener('load', e => {
                 img.addEventListener('click', expand, false);
             }
             galleries.appendChild(h2);
-            galleries.appendChild(document.createElement('hr'));
+            galleries.appendChild(hr);
             galleries.appendChild(gdiv);
             const link = document.createElement('a');
             const span = document.createElement('span');
@@ -54,9 +57,26 @@ window.addEventListener('load', e => {
             artLinks.appendChild(link);
             span.innerText = ' | ';
             artLinks.appendChild(span);
+			// Click link: hide everything then unhide the desired gallery
+			link.addEventListener('click', e => {
+				for (let i=0; i<galleriesJs.length; i++) {
+					galleriesJs[i].h2.classList.add('hidden');
+					galleriesJs[i].hr.classList.add('hidden');
+					galleriesJs[i].gdiv.classList.add('hidden');
+				}
+				h2.classList.remove('hidden');
+				hr.classList.remove('hidden');
+				gdiv.classList.remove('hidden');
+			});
         }
         // Remove last separator
         artLinks.removeChild(artLinks.lastChild);
+		// Hide everything but the first gallery
+		for (let i=1; i<galleriesJs.length; i++) {
+			galleriesJs[i].h2.classList.add('hidden');
+			galleriesJs[i].hr.classList.add('hidden');
+			galleriesJs[i].gdiv.classList.add('hidden');
+		}
     })
     .catch(err => {
         galleries.innerHTML = "<h2 style='text-align: center; color: red;'>Something went wrong loading images!</h2>";
